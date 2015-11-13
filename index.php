e4fbb0f3e5fd5c6a99b906ab481771633d9ec896
aimhighagency <bryan@aimhighagency.com>
2015-11-12 18:02:15 -0800

    initial commit

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+	<head>
+		<title></title>
+		
+		<link rel="stylesheet" href="css/main.css">
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta property="og:title" content="" />
+		<meta property="og:type" content="website" />
+		<meta property="og:url" content="" />
+		<meta property="og:image" content="" />
+
+	</head>
+
+	<body>
+
+		<div id="splash">
+			<div class="inner">
+
+				<div class="header">
+					<h3>World's first <a href="http://www.drinkhacker.com/2015/10/20/tasting-lost-spirits-whiskey-experiments/">flash crafted</a> whiskey.</h3>
+					<h1><img src="/images/Rattleback-logo-rye.svg" alt="Rattleback Logo Rye" /></h1>
+					<h2>(R)evolution of the American Spirit.</h2>
+				</div>
+
+				<div id="signup">					
+					<div id="mc_embed_signup">
+						<form action="//letsrattle.us12.list-manage.com/subscribe/post?u=8fe1fbb9b73c2c893f5489118&amp;id=586909c65d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+						   <div id="mc_embed_signup_scroll">
+							<h3>Be the first to know when Rattleback is unleashed.</h3>
+							<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+						   <div style="position: absolute; left: -5000px;"><input type="text" name="b_8fe1fbb9b73c2c893f5489118_586909c65d" tabindex="-1" value=""></div>
+						    
+						    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+						    </div>
+						</form>
+					</div>
+				</div>
+
+				<div id="social-icons">
+					<h4>You know what to do with these:</h4>
+					<a href="http://twitter.com/RattlebackUS" target="_blank" class="icon-twitter"></a>
+					<a href="http://facebook.com/RattlebackUS" target="_blank" class="icon-facebook"></a>
+					<a href="http://instagram.com/RattlebackUS" target="_blank" class="icon-instagram"></a>
+				</div>
+			</div>
+		</div>
+
+		<div id="age-check">
+			<div class="inner">
+				<div class="content">
+					<div class="header">
+						<h3>First,</h3>
+						<h1>Are you over</h1>
+						<div class="age">21?</div>
+					</div>
+					<p>You must be of legal drinking age <br />to enter this site.</p>
+					<div class="btns"><a href="#" class="yes">yes</a><a href="https://media.giphy.com/media/yJtf2RbatuFag/giphy.gif" class="no">No</a></div>
+				</div>
+			</div>
+		</div>
+
+		<div id="bg-main"></div>
+
+		<script type="text/javascript" src="/js/jquery.min.js" ></script>
+		<script type="text/javascript" src="js/SetCase.js"></script>
+		<script type="text/javascript" src="js/init.js"></script>
+
+	</body>
+
+</html>
